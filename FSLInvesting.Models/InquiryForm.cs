9ba538475a8d1d9b56dbe0aaaf2ca974804527d3
Kaleb Garrett <kalebagarrett@gmail.com>
2025-02-12 16:13:56 -0500

    Added model for form

---
+++ FSLInvesting.Models/InquiryForm.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FSLInvesting.Models;
+
+public class InquiryForm
+{
+    [Required] public string Name { get; set; }
+    [Required] public string Number { get; set; }
+    [Required] public string Email { get; set; }
+    [Required] public string MonthlyPurchase { get; set; }
+    [Required] public bool MonthlyDeals { get; set; }
+    [Required] public string TargetArea { get; set; }
+    [Required] public string BuyingRequirement { get; set; }
+    public string Note { get; set; }
+}
