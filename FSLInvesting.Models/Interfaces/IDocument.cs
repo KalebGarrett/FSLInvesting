84f0f84ec043c8afc116aaee1f3c528f1a90dcee
Kaleb Garrett <kalebagarrett@gmail.com>
2025-02-13 15:42:07 -0500

    Created interface

---
+++ FSLInvesting.Models/Interfaces/IDocument.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FSLInvesting.Models.Interfaces;
+
+public interface IDocument
+{
+    [BsonId] public string Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int Version { get; set; }
+    public bool Deleted { get; set; }
+}
