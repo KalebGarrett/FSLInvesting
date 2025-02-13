9e2f76b7671911509b09e0285cac71d18b0ef41b
Kaleb Garrett <kalebagarrett@gmail.com>
2025-02-13 16:23:46 -0500

    Created an API key filter

---
+++ FSLInvesting.Api/Authentication/ApiKeyFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FSLInvesting.Api.Authentication;
+
+public class ApiKeyFilter : IAuthorizationFilter
+{
+    private readonly IConfiguration _configuration;
+    
+    public ApiKeyFilter(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        try
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue("X-Api-Key",
+                    out var extractedApiKey))
+            {
+                context.Result = new UnauthorizedObjectResult("API key header is missing");
+            }
+            else
+            {
+                var apiKey = _configuration.GetValue<string>("Authentication:ApiKey");
+                if (!apiKey.Equals(extractedApiKey))
+                {
+                    context.Result = new UnauthorizedObjectResult("Invalid API key");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+}
