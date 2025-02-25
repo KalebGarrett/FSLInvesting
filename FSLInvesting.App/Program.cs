6ccd3dc9ec9a12e9519ad105bbf7fb7dcbf0bdd9
Kaleb Garrett <kalebagarrett@gmail.com>
2025-02-25 14:58:08 -0500

    Added MudBlazor nuget package

--- FSLInvesting.App/Program.cs
+++ FSLInvesting.App/Program.cs
@@ -2,13 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FSLInvesting.App.Services;
 using FSLInvesting.App;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri("https://fslinvestingapi.azurewebsites.net/index.html")});
-
 builder.Services.AddScoped<InquiryService>();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
